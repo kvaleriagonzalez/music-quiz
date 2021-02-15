963ee8dc1520dc4b57c47f5956b095d44b0bd28b
karen <k.valeria.gonzalez@gmail.com>
2021-02-15 13:46:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+ $(document).ready(function(){
+
+  let haveWrongAnswer = false;
+  let wrongAnswerCount = 0;
+  let answerCount = 0;
+  let correctAnswer = false;
+  let finalCount = 0;
+
+        
+    const results = function (){
+     $( ".results" ).text(`You have ${answerCount} Answer Correct and ${wrongAnswerCount} Answer Wrong`);
+    }
+    
+    $('.wrong').on('click', function (){
+      haveWrongAnswer = true;
+      wrongAnswerCount = wrongAnswerCount + 1;
+      $(this).css('background-color', 'rgba(255, 117, 107, 0.68)');
+    });
+
+  
+    $('.correct').on('click', function(){
+      correctAnswer = true;
+      answerCount = answerCount + 1;
+      $(this).css('background-color', 'green');
+    })
+
+      $('.wrong , .correct').on('click', function () {
+    let button = this
+    setTimeout(function(){
+      $(button).parent().removeClass('show');
+      $(button).parent().next().addClass("show");
+      results();
+    },500)
+    });
+
+
+});
